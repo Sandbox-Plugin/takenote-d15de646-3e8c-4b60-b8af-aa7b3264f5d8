46bc0b3fae52bc2ca6054e073cdd619d3e898f26
Amin Hosseini <>
2023-10-16 15:29:51 +0300

    suppress webpack errors in the console.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -7,6 +7,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
+  stats: 'none',
   module: {
     rules: [
       // Styles
